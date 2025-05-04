f204335512edff763dcb7d7cdf8fe793ce4d4bf3
Muhanguzi Mordecai <58687171+muhanguzimordecai@users.noreply.github.com>
2025-05-04 12:44:45 +0300

    Delete f1data.py

--- f1data.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.title('F1 Fantasy Analytics')
-data = pd.read_csv('data/latest_results.csv')
-st.line_chart(data.set_index('Race')[['DriverA', 'DriverB']])
